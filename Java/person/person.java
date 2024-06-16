2f59f64b6261d521783a423a4e8803a43e2078c1
Braden Heeney <162655434+MattressBro@users.noreply.github.com>
2024-06-16 15:35:02 -0500

    Add files via upload

---
+++ Java/person/person.java
@@ -0,0 +1,19 @@
+//Braden Heeney
+//Fictional person code 
+//first name, last name, street address, city, and zip code
+
+public class person{
+	public static void main (String [] args){
+		String firstName = "Tom";
+		String lastName = "Riddle";
+		String streetAddress = "9 3/4 Hogwarts School of Witchcraft and Wizardry\nHogwarts Castle";
+		String city = "Scottish Highlands, Great Britian";
+		int zipCode = 12345;
+		
+		System.out.println(firstName);
+		System.out.println(lastName);
+		System.out.println(streetAddress);
+		System.out.println(city);
+		System.out.println(zipCode);
+	}
+}
