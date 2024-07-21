93cc32d9356bda7f1990f5b7dd77411d3658b757
Braden Heeney <bheeney97@gmail.com>
2024-07-21 13:44:10 -0500

    Update vehicleInventory.java

--- portfolioProject/vehicleInventory.java
+++ portfolioProject/vehicleInventory.java
@@ -4,7 +4,7 @@ package portfolioProject;
 //Module 6 milestone portfolio project
 //Submit document with methods for your Automobile class, and pseudo code indicating functionality of each method.
 import java.util.ArrayList;
-public class vehicleInventory{
+public class VehicleInventory{
 
     private ArrayList<Automobile> inventory = new ArrayList<Automobile>();
     public void addAuto(Automobile newAuto){
@@ -14,8 +14,10 @@ public class vehicleInventory{
         inventory.remove(newAuto);
     }
     public void displayInventory(){
-        for (Automobile Automobile : inventory) {
-            
+        for(int i=0; i < inventory.size(); i++){
+            System.out.println("Make: "+inventory.get(i).getMake());
+            System.out.println("Model: "+inventory.get(i).getModel());
+            System.out.println("Color: "+inventory)
         }
     }
 }
