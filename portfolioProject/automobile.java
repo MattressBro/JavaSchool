6188ae04f6380b054950d663928c9b13f1ee8d6e
Braden Heeney <bheeney97@gmail.com>
2024-07-21 12:32:04 -0500

    changed file name

---
+++ portfolioProject/automobile.java
@@ -0,0 +1,22 @@
+//Braden Heeney
+//Module 6 milestone portfolio project
+//Submit document with methods for your automobile class, and pseudo code indicating functionality of each method.
+import java.util.Scanner;
+public class automobile{
+    private String make;
+    private String model;
+    private String color;
+    private int year;
+    private int mileage;
+
+    //default constructor
+    public automobile(){
+        make="";
+        model="";
+        color="";
+        year=9999;
+        mileage=9999;
+    }
+    
+    
+}
--- portfolioProject/vehicleInventory.java
+++ portfolioProject/vehicleInventory.java
@@ -1,1 +1,3 @@
-
+//Braden Heeney
+//Module 6 milestone portfolio project
+//Submit document with methods for your automobile class, and pseudo code indicating functionality of each method.
